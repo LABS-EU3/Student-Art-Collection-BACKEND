c49dde01d55af1a2900107ff0feb80e393a0b689
Petar Scepanovic <hiborija@gmail.com>
2019-12-16 15:12:20 +0100

    database setup

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const db = require('./config/keys').mongoURL
+const db = require('./config/db')
+
 
 const adminRoutes = require('./routes/admin');
 const authRoutes = require('./routes/auth');
@@ -19,7 +20,7 @@ app.use(bodyParser.json());
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+
 
 
 
@@ -30,12 +31,8 @@ app.use(errorController.get404);
 
 const port = process.env.PORT || 3000;
 
+db()
 
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
   .then(() => {
     app.listen(port, () => console.log(`Server running on port ${port}`));
   })
--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
 module.exports = {
+    TEST_DB : process.env.TEST_DB,
+    DATA_DB : process.env.DB,
     mongoURL: "mongodb+srv://petar:admin@artfunder-iayl7.mongodb.net/artfunder?retryWrites=true&w=majority"
 }
