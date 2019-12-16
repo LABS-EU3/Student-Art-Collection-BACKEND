1deedaf918169d1f57b62fda548da0d4b19c3ff1
oloruntobi <awoderuoloruntobi@gmail.com>
2019-12-16 21:30:11 +0100

    Feature: Create JWT token with new user

--- api/helpers/jwt.js
+++ api/helpers/jwt.js
@@ -14,36 +14,7 @@ async function generateToken(user) {
   const token = await jwt.sign(payload, process.env.JWT_Secret, options);
   return token;
 }
-
-async function authorizeUser(req, res, next) {
-  const { token } = req.headers;
-  if (!token) {
-    res.status(401).json({
-      error: "Token is required"
-    });
-  }
-  try {
-    const decode = await jwt.verify(token, process.env.JWT_Secret);
-    req.decode = decode;
-  } catch (error) {
-    res.status(401).json({
-      error: "Token Error"
-    });
-  }
-  try {
-    const { username } = req.params;
-    if (username !== req.decode.username) {
-      res.status(401).json({
-        error: "User denied Access"
-      });
-    }
-    return next();
-  } catch (error) {
-    return next({ message: "Error validating User" });
-  }
-}
-
 module.exports = {
-  generateToken,
-  authorizeUser
+  generateToken
+ 
 };
