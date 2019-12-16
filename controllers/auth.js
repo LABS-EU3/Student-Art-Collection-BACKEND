c2a385a8c896011e0b2578b19ecb02699663df75
Brenda Joshua <brendajosh.bj@gmail.com>
2019-12-17 00:46:59 +0300

    configure login auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,26 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const config = require('../config/db')
 
-exports.getLogin = (req, res, next) => {
-	res.json({ msg: 'Welcome user' });
-};
+	async function logIn(req, res) {
+		User.findOne({
+			username: req.body.username
+		}, (err, user) => {
+			if (err) throw err;
+			if (!user) {
+				res.status(401).send({ success: false, msg: 'Authentication failed. User not found.'});
+			} else {
+				user.comparePassword(req.body.password, (isMatch) => {
+					if (isMatch && !err) {
+						const token = jwt.sign(user.toJSON(), config.secret, {expiresIn: '60d'});
+						res.json({ success: true, token: `JWT${  token}`});
+					} else {
+						res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
+					}
+				});
+			}
+		});
+	}
 
-exports.postLogin = (req, res, next) => {
-	res.json({ msg: 'logged in' });
-};
+
+export default logIn;
