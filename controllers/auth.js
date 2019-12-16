4efcc8b8bfd8c14201fe5f77a35365d56ad14283
oloruntobi <awoderuoloruntobi@gmail.com>
2019-12-16 21:26:13 +0100

    Feature: Deleted some files

--- controllers/auth.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/user');
-
-exports.getLogin = (req, res, next) => {
-	res.json({ msg: 'Welcome user' });
-};
-
-exports.postLogin = (req, res, next) => {
-	res.json({ msg: 'logged in' });
-};
