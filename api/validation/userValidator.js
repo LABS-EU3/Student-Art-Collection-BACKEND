2c215d17f0380039b1a92c78716b0e7594c24450
oloruntobi <awoderuoloruntobi@gmail.com>
2019-12-16 21:28:57 +0100

    Feature: Add validation

--- api/validation/userValidator.js
+++ api/validation/userValidator.js
@@ -1,67 +1,58 @@
+/* eslint-disable object-shorthand */
 const Validator = require("validatorjs");
+const models = require("../../models/user");
 
-async function validateUserEmail(req, res, next) {
-  // Destructure email from request body here
-  const validator = new Validator(req.body, {
-    email: "required|email"
-  });
-  if (validator.fails()) {
-    res.status(400).json({
-      error: "Input a valid email"
-    });
-  }
-  try {
-    // Find user with email here using Model and set model to user
-    //     if (!user) {
-    //       res.status(404).json({
-    //         error: "User not found"
-    //       });
-    //     }
-    //     req.userEmail = user;
-    return next();
-  } catch (error) {
-    return next({ message: "Server error try again" });
-  }
-}
-
-function validatePassword(req, res, next) {
-  const { body } = req;
-  const validator = new Validator(body, {
-    password: "required|min:5"
-  });
-  if (validator.fails()) {
-    res.status(400).json({
-      error: "Password must be at least 5 characters"
-    });
-  }
-  return next();
-}
-
-async function validateUserOnSignup(req, res, next) {
-  const validator = new Validator(req.body, {
-    password: "required|min:8",
-    email: "required|email"
-  });
-
-  if (validator.fails()) {
-    res.status(400).json({
-      errors: validator.errors.all()
-    });
-  }
-  try {
-    // await Find user
-    // if (!user) {
-    //   return next();
-    // }
-    return res.status(400).json({
-      error: "User already registered with username or email provided"
-    });
-  } catch (error) {
-    return next({ message: "Error validating user signup" });
-  }
-}
 module.exports = {
-  validateUserEmail,
-  validatePassword,
-  validateUserOnSignup
+  async validateUserEmail(req, res, next) {
+    const { email } = req.body;
+    const validator = new Validator(req.body, {
+      email: "required|email"
+    });
+    if (validator.fails()) {
+     return res.status(400).json({
+        error: "Input a valid email"
+      });
+    }
+    try {
+      const user = await models.findOne({ email: email });
+      if (!user) {
+       return res.status(404).json({
+          error: "User not found"
+        });
+      }
+      req.userEmail = user;
+      return next();
+    } catch (error) {
+      return next({ message: "Server error try again" });
+    }
+  },
+
+
+  async validateUserOnSignup(req, res, next) {
+    const validator = new Validator(req.body, {
+      password: "required|min:8",
+      email: "required|email"
+    });
+
+    if (validator.fails()) {
+    return  res.status(400).json({
+        errors: validator.errors.all()
+      });
+    }
+    try {
+      const user = await models.findOne({ email: req.body.email });
+      if (!user) {
+        return next();
+      }
+      return res.status(400).json({
+        error: "User already registered with username or email provided"
+      });
+    } catch (error) {
+      return next({ message: "Error validating user signup" });
+    }
+  }
 };
+// eslint-disable-next-line func-names
+process.on('unhandledRejection', function(err) {
+  console.log(err);
+});
