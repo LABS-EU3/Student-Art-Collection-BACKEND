925a48ef2c2b007a6a082fea838af00a62ca47aa
oloruntobi <awoderuoloruntobi@gmail.com>
2019-12-16 21:32:47 +0100

    Feature: Add it to index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./config/db');
+const UserauthRoute = require('./api/routes/authroute')
+
+const Port = process.env.PORT 
 
 const app = express();
 
 app.use(express.json());
-const Port = process.env.PORT || 4000;
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', UserauthRoute)
+
+app.use(express.json());
+
+
+
 
 // eslint-disable-next-line no-console
-app.listen(() => console.log(`listening on ${Port}`));
+db()
+	.then(() => {
+		app.listen(Port, () => console.log(`Server running on port ${Port}`));
+	})
+	.catch((err) => {
+		console.log(err);
+	});
