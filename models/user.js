b58e6c9fdca69d7c11bbbef1cde386faab635224
iamcristos <nmereginivincent@gmail.com>
2019-12-17 17:34:10 +0100

    added enums type

--- models/user.js
+++ models/user.js
@@ -33,6 +33,11 @@ const userSchema = new Schema({
     },
     location:{
         type: String
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ['school', 'buyer']
     }
 }, {timestamps: true});
 
