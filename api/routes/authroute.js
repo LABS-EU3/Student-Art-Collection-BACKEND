4ac405f2abad50f919b793acbb95ae362a3b1a2f
Brenda Joshua <brendajosh.bj@gmail.com>
2019-12-17 18:27:24 +0300

    return status 200 for token

--- api/routes/authroute.js
+++ api/routes/authroute.js
@@ -6,6 +6,7 @@ const userValidators = require('../validation/userValidator');
 const router = express.Router();
 // eslint-disable-next-line no-unused-vars
 router.post("/signup",[userValidators.validateUserOnSignup], controller.createUser);
+router.post("/login")
 
 
 module.exports = router;
