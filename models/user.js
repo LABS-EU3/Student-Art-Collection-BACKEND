18e3a133747068e40188bb96a304b08a58c730b0
Petar Scepanovic <hiborija@gmail.com>
2019-12-18 17:16:55 +0100

    update

--- models/user.js
+++ models/user.js
@@ -3,46 +3,47 @@ const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-	email: {
-		type: String,
-        required: [true, 'User must have a email'],
-        unique: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: [true, 'User must have a password'],
-    },
-    confirmed: {
-        type: Boolean,
-        default: false
-    },
-    profile_picture: {
-        type: String,
-        trim: true
-    },
-    location:{
-        type: String
-    },
-    type: {
-        type: String,
-        required: true,
-        enum: ['school', 'buyer']
-    }
-}, {timestamps: true});
+const userSchema = new Schema(
+	{
+		email: {
+			type: String,
+			required: [ true, 'User must have a email' ],
+			unique: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: [ true, 'User must have a password' ],
+		},
+		confirmed: {
+			type: Boolean,
+			default: false,
+		},
+		profile_picture: {
+			type: String,
+			trim: true,
+		},
+		location: {
+			type: String,
+		},
+		type: {
+			type: String,
+			required: true,
+			enum: [ 'school', 'buyer' ],
+		},
+	},
+	{ timestamps: true },
+);
 
 userSchema.methods.comparePassword = (password) => {
-    const user = bcrypt.compareSync(password, this.password);
-    return user ? this : null;
-  };
-
-
+	const user = bcrypt.compareSync(password, this.password);
+	return user ? this : null;
+};
 
 userSchema.pre('save', (next) => {
-    const hashPassword = bcrypt.hashSync(this.password, 10);
-    this.password = hashPassword
-    next()
-})
+	const hashPassword = bcrypt.hashSync(this.password, 10);
+	this.password = hashPassword;
+	next();
+});
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema);
