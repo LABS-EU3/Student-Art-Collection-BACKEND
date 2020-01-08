c98dad31b47b225a6e6ffc4c1fc8a49ee1ffd9cb
oloruntobi <awoderuoloruntobi@gmail.com>
2020-01-08 16:15:30 +0100

    Chore: import Shemas to index file

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const User = require('./user');
 const Buyer = require('./buyer');
 const School = require('./school');
+const Products = require('./product')
+const Transaction = require('./transaction')
 
 module.exports = {
-    User, Buyer, School
+    User, Buyer, School, Products, Transaction
 }
