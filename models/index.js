e09130702b24e53434f2403ff617571775bef707
oloruntobi <awoderuoloruntobi@gmail.com>
2020-01-09 11:43:18 +0100

    Chore: Made recommended changes

--- models/index.js
+++ models/index.js
@@ -3,7 +3,8 @@ const Buyer = require('./buyer');
 const School = require('./school');
 const Products = require('./product')
 const Transaction = require('./transaction')
+const order  = require('./orders')
 
 module.exports = {
-    User, Buyer, School, Products, Transaction
+    User, Buyer, School, Products, Transaction, order
 }
