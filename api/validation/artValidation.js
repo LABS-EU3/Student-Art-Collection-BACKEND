4bc1de8eb519f2b5ed592ec5cc4b521996055a50
Pere <pere.solaclaver@gmail.com>
2020-01-10 13:55:12 +0000

    Add validatorjs code inside artvalidation middleware and return object with error messages by attribute

--- api/validation/artValidation.js
+++ api/validation/artValidation.js
@@ -1,3 +1,6 @@
+// DEPENDENCIES
+const Validator = require('validatorjs');
+
 // MODELS
 const models = require('../../models/index');
 
@@ -6,11 +9,16 @@ const { errorHelper } = require('../helpers/response');
 
 module.exports = {
   validateArtBody(req, res, next) {
-    const { name, height, width, quantity, price, picture } = req.body;
-    if (!name || !height || !width || !quantity || !price || !picture) {
-      errorHelper(res, 401, { message: 'Please provide all required fields' });
-    } else {
-      next();
-    }
+    const validator = new Validator(req.body, {
+      name: 'required',
+      height: 'required',
+      width: 'required',
+      quantity: 'required',
+      price: 'required',
+      picture: 'required'
+    });
+    if (validator.fails()) {
+      errorHelper(res, 401, { message: validator.errors.all() });
+    } else next();
   }
 };
