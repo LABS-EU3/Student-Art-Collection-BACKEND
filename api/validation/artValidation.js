e9c8529ae10305f602fc305d4f489efb6a8d7690
Pere <pere.solaclaver@gmail.com>
2020-01-10 13:10:40 +0000

    Add required picture to the product model and to the check required fields middleware

--- api/validation/artValidation.js
+++ api/validation/artValidation.js
@@ -6,8 +6,8 @@ const { errorHelper } = require('../helpers/response');
 
 module.exports = {
   validateArtBody(req, res, next) {
-    const { name, height, width, quantity, price } = req.body;
-    if (!name || !height || !width || !quantity || !price) {
+    const { name, height, width, quantity, price, picture } = req.body;
+    if (!name || !height || !width || !quantity || !price || !picture) {
       errorHelper(res, 401, { message: 'Please provide all required fields' });
     } else {
       next();
--- models/product.js
+++ models/product.js
@@ -1,50 +1,56 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: {
-		type: String,
-        required: [true, 'Product must have a name'],
-        trim: true
+      type: String,
+      required: [true, 'Product must have a name'],
+      trim: true
     },
     medium: {
-        type: String
+      type: String
     },
     subject: {
-        type: String
+      type: String
     },
     height: {
-        type: Number,
-        required: [true, 'Product must have a height']
+      type: Number,
+      required: [true, 'Product must have a height']
     },
     width: {
-        type: Number,
-        required: [true, 'Product must have a width']
+      type: Number,
+      required: [true, 'Product must have a width']
     },
     style: {
-        type: String
+      type: String
     },
     category: {
-        type: String
+      type: String
     },
     materials: {
-        type: String
+      type: String
     },
     quantity: {
-        type: Number,
-        required: [true, 'Product must have a quantity']
+      type: Number,
+      required: [true, 'Product must have a quantity']
     },
     price: {
-        type: Number,
-        required:[true, 'Product must have a price']
+      type: Number,
+      required: [true, 'Product must have a price']
     },
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user'
     },
     description: {
-        type: String
+      type: String
     },
-   
-}, {timestamps: true});
+    picture: {
+      type: String,
+      required: [true, 'Products must have a picture']
+    }
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('product', productSchema)
+module.exports = mongoose.model('product', productSchema);
