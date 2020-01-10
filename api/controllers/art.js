22c760dc5b8249f574807daa1f6a1453d393dc29
Pere <pere.solaclaver@gmail.com>
2020-01-10 09:50:44 +0000

    Test art endpoint with response helpers

--- api/controllers/art.js
+++ api/controllers/art.js
@@ -1,8 +1,11 @@
 // MODELS
 const models = require('../../models/index');
 
+// HELPERS
+const { successResponse, errorHelper } = require('../helpers/response');
+
 module.exports = {
   testArt(req, res) {
-    res.status(200).json({ message: 'hello from endpoint' });
+    return successResponse(res, 200, { message: 'hello from endpoint' });
   }
 };
