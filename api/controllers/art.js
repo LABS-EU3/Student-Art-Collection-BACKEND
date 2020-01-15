87460267131f43b556bf077960c63fcf17bd3f38
oloruntobi <awoderuoloruntobi@gmail.com>
2020-01-15 13:22:08 +0100

    Feature: Added reduceArt and deleteArt Controllers

--- api/controllers/art.js
+++ api/controllers/art.js
@@ -7,28 +7,27 @@ const secret = require("../../config/keys");
 module.exports = {
   async markArtAsCollected(req, res, next) {
     const { user } = req;
- 
-    const { id }  = req.params;
 
+    const { id } = req.params;
 
     try {
       const objectId = mongoose.Types.ObjectId(id.toString());
-     
+
       const transaction = await models.Transaction.findById(objectId)
         .populate("buyerId")
         .populate("productId");
-     
+
       const order = await models.order.findOneAndUpdate(
         { transactionId: objectId },
         { status: "completed" },
         { new: true }
       );
-      
+
       artMail(
         secret.FRONTEND_BASE_URL,
         user.email,
         transaction.buyerId.firstname,
-        transaction.productId 
+        transaction.productId
       );
 
       return successResponse(res, 200, order);
@@ -57,19 +56,63 @@ module.exports = {
 
   async artSoldCollection(req, res, next) {
     const { id } = req.params;
-    const {status} = req.query;
+    const { status } = req.query;
     try {
       let schoolOrders = null;
-      if(status === 'all') {
-        schoolOrders = await  models.order.find({schoolId: id})
-        .populate('transactionId').populate('buyerId').exec();
-      }else {
-        schoolOrders = await models.order.find({schoolId: id, status})
-          .populate('transactionId').populate('buyerId').exec();
+      if (status === "all") {
+        schoolOrders = await models.order
+          .find({ schoolId: id })
+          .populate("transactionId")
+          .populate("buyerId")
+          .exec();
+      } else {
+        schoolOrders = await models.order
+          .find({ schoolId: id, status })
+          .populate("transactionId")
+          .populate("buyerId")
+          .exec();
       }
       return successResponse(res, 200, schoolOrders);
     } catch (error) {
-      return next(error)
+      return next(error);
+    }
+  },
+
+  async deleteArt(req, res, next) {
+    const { id } = req.params;
+
+    const { user } = req;
+    try {
+      const objectId = mongoose.Types.ObjectId(id.toString());
+      const isArt = await models.Transaction.findOne({ productId: objectId });
+      if (isArt) {
+        return errorHelper(res, 500, "You cannot delete this product");
+      }
+
+      const remove = await models.Products.deleteOne({ _id: objectId });
+      return remove;
+    } catch (error) {
+      return next(error);
+    }
+  },
+  async reduceArtQuantity(req, res, next) {
+    const { id } = req.params;
+    const { user } = req;
+    try {
+      const objectId = mongoose.Types.ObjectId(id.toString());
+      const product = await models.Products.findById(objectId);
+      const quantity = product.quantity;
+      if (quantity >= 1) {
+        return models.Products.findByIdAndUpdate(
+          objectId,
+          { quantity: quantity - 1 },
+          { new: true }
+        );
+      }
+      errorHelper(res, 500, "Art is no longer for sale");
+      return user;
+    } catch (error) {
+      return next(error);
     }
   }
 };
