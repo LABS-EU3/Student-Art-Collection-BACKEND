c7682ea12f425cb8164d6ccb0de52aa4fc49d996
Pere <pere.solaclaver@gmail.com>
2020-01-23 13:38:55 +0000

    Create env variable with Stripe APi key and rolled Key because previous one was pushed to github by mistake

--- api/controllers/payments.js
+++ api/controllers/payments.js
@@ -1,5 +1,7 @@
+const config = require('../../config/keys');
+
 // DEPENDENCIES
-const stripe = require('stripe')('sk_test_aMqnc5Qsd2DQrAB48V1dYljL00w2vvfZ63');
+const stripe = require('stripe')(config.STRIPE_API_KEY);
 const mongoose = require('mongoose');
 
 // MODELS
@@ -15,12 +17,35 @@ module.exports = {
         code: authCode
       });
       const objectId = mongoose.Types.ObjectId(id.toString());
-      const school = await models.School.findByIdAndUpdate(objectId, {
-        stripe_user_id: response.stripe_user_id
-      }, {new: true});
+      const school = await models.School.findByIdAndUpdate(
+        objectId,
+        {
+          stripe_user_id: response.stripe_user_id
+        },
+        { new: true }
+      );
       res.status(200).json(school);
     } catch (error) {
       res.status(500).json(error.message);
     }
   }
+
+  //   async makePaymentConnectedAccount(req, res, next) {
+  //     const { price, currency, stripeUserId } = req.body;
+  //     try {
+  //       const paymentIntent = stripe.paymentIntents.create(
+  //         {
+  //           payment_method_types: ['card'],
+  //           amount: price * 100,
+  //           currency
+  //         },
+  //         {
+  //           stripe_account: stripeUserId
+  //         }
+  //       );
+  //       res.status(200).json(paymentIntent);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
 };
