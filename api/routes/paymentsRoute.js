a572a07258693e70a23da0aa18bebaceb3d7c252
Pere <pere.solaclaver@gmail.com>
2020-01-23 13:10:00 +0000

    Add user validator middleware to endpoint to fetch stripe account id

--- api/routes/paymentsRoute.js
+++ api/routes/paymentsRoute.js
@@ -4,12 +4,16 @@ const express = require('express');
 // CONTROLLERS
 const paymentscontroller = require('../controllers/payments');
 
+// VALIDATORS
+const userValidator = require('../validation/userValidator');
+
 const router = express.Router();
 
 router.use(express.json());
 
 router.post(
   '/fetchcredentials/:id',
+  [userValidator.validateUser],
   paymentscontroller.fetchConnectedAccountCredentials
 );
 
