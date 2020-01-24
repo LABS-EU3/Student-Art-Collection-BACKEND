3fc18bb7acf43a4d02ae3d6d8db46324fa09225b
Pere <pere.solaclaver@gmail.com>
2020-01-24 15:34:10 +0000

    Wire up payment confirmation email and payment failure email

--- api/helpers/artmail.js
+++ api/helpers/artmail.js
@@ -1,71 +1,136 @@
-const nodemailer = require("nodemailer");
-const Mailgen = require("mailgen");
-const secret = require("../../config/keys");
-const { intro,  outro, subject } = require("./mailText");
+const nodemailer = require('nodemailer');
+const Mailgen = require('mailgen');
+const secret = require('../../config/keys');
+const { intro, outro, subject } = require('./mailText');
 
-async function artMail(url, email, name, product) {
-  const mailGenerator = new Mailgen({
-    theme: "default",
-    product: {
-      name: "ArtFinder",
-      link: `${url}`
-    }
-  });
-  const mail = {
-    body: {
-      name,
-      intro: intro.third,
-      table: {
-        data: [
-          {
-            item: product.name,
-            description: product.description,
-            price: product.price
+module.exports = {
+  async artPurchaseConfirmationMail(url, email, name, product) {
+    const mailGenerator = new Mailgen({
+      theme: 'default',
+      product: {
+        name: 'ArtFinder',
+        link: `${url}`
+      }
+    });
+    const mail = {
+      body: {
+        name,
+        intro: intro.third,
+        table: {
+          data: [
+            {
+              item: product.name,
+              description: product.description,
+              price: product.price
+            }
+          ],
+          columns: {
+            customWidth: {
+              item: '20%',
+              price: '15%'
+            },
+
+            customAlignment: {
+              price: 'right'
+            }
           }
-        ],
-        columns: {
-          customWidth: {
-            item: "20%",
-            price: "15%"
-          },
+        },
 
-          customAlignment: {
-            price: "right"
+        outro: outro.third
+      }
+    };
+    const emailBody = mailGenerator.generate(mail);
+
+    const emailText = mailGenerator.generatePlaintext(mail);
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      // requireTLS: true
+      auth: {
+        user: secret.USER_MAIL,
+        pass: secret.PASSWORD_MAIL
+      }
+    });
+
+    const mailOption = {
+      from: 'studentartcollectionlabseu3@gmail.com',
+      to: email,
+      subject: subject.fourth,
+      html: emailBody,
+      text: emailText
+    };
+
+    try {
+      const passwordMail = await transporter.sendMail(mailOption);
+      return passwordMail;
+    } catch (error) {
+      return error.message;
+    }
+  },
+
+  async artPurchaseFailureMail(url, email, name, product) {
+    const mailGenerator = new Mailgen({
+      theme: 'default',
+      product: {
+        name: 'ArtFinder',
+        link: `${url}`
+      }
+    });
+    const mail = {
+      body: {
+        name,
+        intro: intro.fourth,
+        table: {
+          data: [
+            {
+              item: product.name,
+              description: product.description,
+              price: product.price
+            }
+          ],
+          columns: {
+            customWidth: {
+              item: '20%',
+              price: '15%'
+            },
+
+            customAlignment: {
+              price: 'right'
+            }
           }
-        }
-      },
-     
-      outro: outro.third
+        },
+
+        outro: outro.fourth
+      }
+    };
+    const emailBody = mailGenerator.generate(mail);
+
+    const emailText = mailGenerator.generatePlaintext(mail);
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      // requireTLS: true
+      auth: {
+        user: secret.USER_MAIL,
+        pass: secret.PASSWORD_MAIL
+      }
+    });
+
+    const mailOption = {
+      from: 'studentartcollectionlabseu3@gmail.com',
+      to: email,
+      subject: subject.fifth,
+      html: emailBody,
+      text: emailText
+    };
+
+    try {
+      const passwordMail = await transporter.sendMail(mailOption);
+      return passwordMail;
+    } catch (error) {
+      return error.message;
     }
-  };
-  const emailBody = mailGenerator.generate(mail);
-
-  const emailText = mailGenerator.generatePlaintext(mail);
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    // requireTLS: true
-    auth: {
-      user: secret.USER_MAIL,
-      pass: secret.PASSWORD_MAIL
-    }
-  });
-
-  const mailOption = {
-    from: "studentartcollectionlabseu3@gmail.com",
-    to: email,
-    subject: subject.Fourth,
-    html: emailBody,
-    text: emailText
-  };
-
-  try {
-    const passwordMail = await transporter.sendMail(mailOption);
-    return passwordMail;
-  } catch (error) {
-    return error.message;
   }
-}
-
-module.exports = artMail ;
+};
--- api/helpers/mailText.js
+++ api/helpers/mailText.js
@@ -7,6 +7,7 @@ module.exports = {
         first: 'You have received this email because you just signup at ArtFinder',
         second: 'You have received this email because a password reset request for your account was received.',
         third: "Your order has been processed successfully.",
+        fourth: "There was a problem with your order"
     },
     instructions: {
         first:  'Click the button below to confirm your account',
@@ -22,12 +23,14 @@ module.exports = {
     outro : {
         first: 'If you did not signup to ArtFinder, no further action is required on your part.',
         second:  'If you did not request a password reset, no further action is required on your part.',
-        third: "Thank You for your purchase"
+        third: "Thank You for your purchase. The school will get in touch with you shortly to arrange collection of the item purchased.",
+        fourth: "Your payment was nt succesful. Please try again."
     },
     subject : {
         first: "Confirm your email",
         second:  'Password Reset',
-        Fourth: "Art Delivery",
-        third: "Customer Query"
+        third: "Customer Query",
+        fourth: "Art Delivery - payment confirmed",
+        fifth: "Art Delivery - payment was not succesful"
     }
 }
