7ca87741d62033d63ac9b62e57f48ec23e6366ad
oloruntobi <awoderuoloruntobi@gmail.com>
2020-01-29 18:37:58 +0100

    Chore: Changed Userschema

--- models/user.js
+++ models/user.js
@@ -1,68 +1,81 @@
 /* eslint-disable func-names */
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const Schema = mongoose.Schema;
 
-
-const userSchema = new Schema({
-	email: {
-		type: String,
-        required: [true, 'User must have a email'],
-        unique: true,
-        trim: true
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "User must have a email"],
+      unique: true,
+      trim: true
     },
     password: {
-        type: String,
-       
+      type: String
     },
     confirmed: {
-        type: Boolean, 
-        default: false
+      type: Boolean,
+      default: false
     },
     profile_picture: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true
     },
-    location:{
-        type: String
+    location: {
+      type: String
     },
     type: {
-        type: String,
-        required: true,
-        enum: ['school', 'buyer']
+      type: String,
+      required: true,
+      enum: ["school", "buyer"]
     },
     reset_password_token: {
-        type: String
+      type: String
     },
     reset_password_expires: {
-        type: Number
+      type: Number
     },
     auth_id: {
-        type: String
-    },
-}, {timestamps: true});
+      type: String
+    }
+  },
+  { timestamps: true }
+);
 
-
-userSchema.add({userLocation: {
+userSchema.add({
+  userLocation: {
     name: String,
     administrative: String,
     country: String,
-    latitude: Number,
-    longitude: Number,
-    postCode: String
-}})
-userSchema.methods.comparePassword = function(password) {
-	const user = bcrypt.compareSync(password, this.password);
-	return user ? this : null;
-};
 
-userSchema.pre('save', function(next){
-	if(!this.confirmed) {
-		const hashPassword = bcrypt.hashSync(this.password, 10);
-		this.password = hashPassword;
-	}
-	next();
+    postCode: String,
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true
+      },
+      coordinates: {
+        type: [Number],
+        required: true
+      }
+    }
+  }
 });
 
-module.exports = mongoose.model('user', userSchema);
+userSchema.methods.comparePassword = function(password) {
+  const user = bcrypt.compareSync(password, this.password);
+  return user ? this : null;
+};
+
+userSchema.pre("save", function(next) {
+  if (!this.confirmed) {
+    const hashPassword = bcrypt.hashSync(this.password, 10);
+    this.password = hashPassword;
+  }
+  next();
+});
+
+module.exports = mongoose.model("user", userSchema);
